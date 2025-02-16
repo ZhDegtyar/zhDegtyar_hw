83c103725c7465211f290002735ac86e5e8a1ec4
zhdegtyar <zhdegtyar@gmail.com>
2025-02-16 19:54:12 +0300

    hw1

--- src/hw1.java
+++ src/hw1.java
@@ -1,7 +1,7 @@
 public class hw1 {
     public static void main(String[] args) {
 
-        System.out.println("Hello, Kirill! It's my hw from 06/02/25");
+        System.out.println("Hello, Kirill! It's my hw 06/02/25");
 
     }
 }
